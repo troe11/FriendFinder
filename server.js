0bececce1e5deca7a6d350cf2798ca3287763ef6
Travis Roemhild <travis.roemhild@gmail.com>
2018-02-12 14:02:17 -0700

    changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var path = require("path");// Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.port || 8080;
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
